5c7909b84812b2bb2ab5abfa2dfddf8597686442
Hyunje Jo <retriever89@gmail.com>
2015-01-21 18:07:04 +0900

    added README.md

--- src/test/java/com/hyunje/jo/NounExtractorTest.java
+++ src/test/java/com/hyunje/jo/NounExtractorTest.java
@@ -9,6 +9,6 @@ package com.hyunje.jo;
 public class NounExtractorTest {
     public static void main(String[] args){
         NounExtractor extractor = new NounExtractor();
-        System.out.println(extractor.extract("트위터 형태소 분석기를 이용한 명사 추출 예시입니다."));
+        System.out.println(extractor.extract("형태소분석기테스트클래스입니다.띄어쓰기가안되어있는경우에는어떤결과가나올까요?"));
     }
 }
