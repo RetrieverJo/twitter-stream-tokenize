e8d0a18bfe621008f566e482e940d9e8620942d9
Hyunje Jo <retriever89@gmail.com>
2015-01-22 00:01:05 +0900

    added comments

--- src/main/java/com/hyunje/jo/NounExtractor.java
+++ src/main/java/com/hyunje/jo/NounExtractor.java
@@ -10,17 +10,25 @@ import java.util.List;
 
 /**
  * 문장에서 명사만 추출하여 반환하는 클래스.
+ * 혹은, JSON 형태로 되어 있는 명사의 리스트에서 JSON을 파싱하여 명사의 리스트를 반환한다.
  *
  * @author hyunje
  * @since 15. 1. 8.
  */
 public final class NounExtractor {
-    private TwitterKoreanProcessorJava processor;
+    private final TwitterKoreanProcessorJava processor;
+    private final String jsonObjectName = "noun";
 
     public NounExtractor() {
         processor = new TwitterKoreanProcessorJava.Builder().build();
     }
 
+    /**
+     * 문장에서 명사를 추출하여 반환한다.
+     *
+     * @param sentence 형태소 분석을 수행할 문장.
+     * @return 명사의 리스트.
+     */
     public List<String> extractFromSentence(String sentence) {
         List<KoreanTokenizer.KoreanToken> parsed = processor
                 .tokenize(sentence);
@@ -33,9 +41,16 @@ public final class NounExtractor {
         return nouns;
     }
 
+    /**
+     * JSON 형태의 문자열 명사만 가져와 반환한다.
+     * {"noun":["나라","걱정","얘기"]} 형태의 JSON을 파싱
+     *
+     * @param jsonString Json 형태의 문자열.
+     * @return 파싱된 명사의 리스트.
+     */
     public List<String> extractFromJSON(String jsonString){
         JSONObject jsonObject = new JSONObject(jsonString);
-        JSONArray nouns = jsonObject.getJSONArray("noun");
+        JSONArray nouns = jsonObject.getJSONArray(jsonObjectName);
 
         List<String> nounArray = new ArrayList<String>();
         for (int i = 0; i < nouns.length(); i++) {
